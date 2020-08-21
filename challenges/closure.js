af65a3185410b81f4d9996768e2346949ca44809
Sahar Jafari <shr.jafari@gmail.com>
2020-08-21 09:16:03 -0700

    Closure

--- challenges/closure.js
+++ challenges/closure.js
@@ -13,10 +13,20 @@ function myFunction() {
   }
   nestedFunction();
 }
-// myFunction();
+myFunction();
 
 // Explanation:
+// Functions and methods have access to variables that exist in the same level scope or in a higher level scope. The `nestedFunction` and `internal` both belong to the scope of `myFunction` and that's why the `nestedFunction` can access the variable `internal`.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(param) {
+  let counter = 0;
+  for (let i = 1; i <= param; i++) {
+    counter += i;
+  }
+  return counter;
+}
+console.log(summation(4));
