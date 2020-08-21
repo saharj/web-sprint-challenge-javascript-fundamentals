ea904d919be2fc298782508655c9e26621e1bad9
Sahar Jafari <shr.jafari@gmail.com>
2020-08-21 10:37:05 -0700

    Stretch challenge

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -119,9 +119,9 @@ console.log(populationTotal);
  * The consume function should return the invocation of cb, passing a and b into cb as arguments
  */
 
-function consume(a, b, cb) {
+const consume = (a, b, cb) => {
   return cb(a, b);
-}
+};
 
 /* Step 2: Create several functions to callback with consume();
  * Create a function named add that returns the sum of two numbers
@@ -129,15 +129,15 @@ function consume(a, b, cb) {
  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
  */
 
-function add(num1, num2) {
+const add = (num1, num2) => {
   return num1 + num2;
-}
-function multiply(num1, num2) {
+};
+const multiply = (num1, num2) => {
   return num1 * num2;
-}
-function greeting(first, last) {
+};
+const greeting = (first, last) => {
   return `Hello ${first} ${last}, nice to meet you!`;
-}
+};
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 console.log(consume(2, 2, add)); // 4
